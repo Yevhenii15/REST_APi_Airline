1d3cb3c527a271e8573fbf63d2c0d4cb2aa453ac
Yevhenii <yevmot01@easv365.dk>
2025-02-25 11:20:58 +0100

    add all

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+import express, { Application } from "express";
+import dotenvFlow from "dotenv-flow";
+import cors from "cors";
+import { testConnection } from "./repository/database";
+import routes from "./routes";
+
+dotenvFlow.config();
+
+// Create express application
+const app: Application = express();
+
+export function setupCors() {
+  // kw 2-dec-2024 - Working CORS setup without credentials. Could refactor
+  app.use(
+    cors({
+      origin: "*", // Allow requests from any origin
+      // kw 29-nov-2024 - allow methods + headers + credentials
+      methods: "GET,HEAD,PUT,OPTIONS,PATCH,POST,DELETE",
+      allowedHeaders: [
+        "auth-token",
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+      ], // Allow specific headers
+      credentials: true,
+    })
+  );
+}
+/**
+ * Starts the Express server
+ */
+export function startServer() {
+  setupCors();
+
+  app.use(express.json());
+
+  // Bind the routes to the application
+  app.use("/api", routes);
+
+  // Test the connection to the database
+  testConnection();
+
+  // Start the server
+  const PORT: number = parseInt(process.env.PORT as string) || 4000;
+  app.listen(PORT, function () {
+    console.log("Server is up and running on port: " + PORT);
+  });
+}
